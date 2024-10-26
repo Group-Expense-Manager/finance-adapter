16f6e6ec6d9b78b678dd1b38596402b5a42563c8
Paweł Gacek <pawelgacek.pl@gmail.com>
2024-10-26 13:11:10 +0200

    GEM-231 | add iternal settlements endpoint

--- src/main/kotlin/pl/edu/agh/gem/external/controller/InternalFinanceController.kt
+++ src/main/kotlin/pl/edu/agh/gem/external/controller/InternalFinanceController.kt
@@ -8,8 +8,10 @@ import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import pl.edu.agh.gem.external.dto.finance.BalancesResponse
 import pl.edu.agh.gem.external.dto.finance.InternalActivitiesResponse
+import pl.edu.agh.gem.external.dto.finance.SettlementsResponse
 import pl.edu.agh.gem.external.dto.finance.toBalancesResponse
 import pl.edu.agh.gem.external.dto.finance.toInternalActivitiesResponse
+import pl.edu.agh.gem.external.dto.finance.toSettlementsResponse
 import pl.edu.agh.gem.internal.service.FinanceService
 import pl.edu.agh.gem.media.InternalApiMediaType.APPLICATION_JSON_INTERNAL_VER_1
 import pl.edu.agh.gem.paths.Paths.INTERNAL
@@ -34,5 +36,13 @@ class InternalFinanceController(
         @PathVariable groupId: String,
     ): BalancesResponse {
         return financeService.getBalances(groupId).toBalancesResponse()
+    }
+
+    @GetMapping("settlements/groups/{groupId}", produces = [APPLICATION_JSON_INTERNAL_VER_1])
+    @ResponseStatus(OK)
+    fun getSettlements(
+        @PathVariable groupId: String,
+    ): SettlementsResponse {
+        return financeService.getSettlements(groupId).toSettlementsResponse()
     }
 }
